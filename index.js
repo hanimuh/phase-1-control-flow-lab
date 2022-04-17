8f95427412b840b936da81da258293259eee1d46
Hani <md.hani@hotmail.com>
2022-04-18 01:49:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!"
+  } else if (2000> ride && ride> 400){
+    return "That will be twenty bucks."
+  } else if (2500> ride && ride > 2000){
+    return "I will gladly take your thirty bucks."
+  } else{ return "No can do."}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good." : "No go."
+
   // Write your code here!
 }
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+    }
 
-function switchOnCharmFromTip(){
   // Write your code here!
 }
